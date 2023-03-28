28f662a0911ccbe58750d5d720332354e33a19ab
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2023-03-28 16:21:49 +0800

    [B-1-0-NN-003-F]修改settings.py(#3)

--- Aplus/settings.py
+++ Aplus/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 import yaml
 
-file = open("config.yml", "r", encoding="utf-8")
+file = open("../config.yml", "r", encoding="utf-8")
 env = yaml.load(file, Loader=yaml.SafeLoader)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
