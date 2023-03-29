f6821ff70062e7b858b0082975418b97e5438cbf
韩宇轩 <hanyx21@mails.tsinghua.edu.cn>
2023-03-29 14:47:38 +0800

    [B-1-1-SS-004-B]删除无用脚本models.sh(#4)

--- models.sh
+++
@@ -1,7 +0,0 @@
-﻿#!/bin/sh
-python3 manage.py makemigrations user
-python3 manage.py makemigrations asset
-python3 manage.py makemigrations department
-python3 manage.py makemigrations pending
-python3 manage.py makemigrations logs
-python3 manage.py migrate
