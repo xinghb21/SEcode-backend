34e344f863d0310acd51b28c93609b30f19b92ad
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2023-04-05 15:43:08 +0800

    [B-2-0-EP-008-I]修改asset数据模型,增加资产类别数据模型,实现增加资产和查看资产的接口(#8)

--- asset/models.py
+++ asset/models.py
@@ -1,29 +1,46 @@
 from utils import utils_time
 from django.db import models
 from utils.utils_request import return_field
+from department.models import Department, Entity
+from user.models import User
 
 # Create your models here.
 class Asset(models.Model):
+    
+    class AsserStatus(models.IntegerChoices):
+        IDLE = 0
+        INUSE = 1
+        MAINTAIN = 2
+        CLEAR = 3
+        DELETE = 4
+    
     #资产id，主键自增
     id = models.BigAutoField(primary_key=True)
     
-    #上级资产id，根节点为0
-    parent = models.BigIntegerField(default=0)
+    #上级资产id，可以没有上级资产
+    parent = models.ForeignKey('asset.Asset', null=True, on_delete=models.SET_NULL)
+    
+    # 资产所属的业务实体
+    # 资产必须属于某一个业务实体，实体被删除时，该实体下必须没有资产
+    entity = models.ForeignKey('department.Entity', on_delete=models.PROTECT)
+    
+    # 资产类别
+    # 资产必须属于某一个类别，类别被删除时，该类别下必须没有资产
+    category = models.ForeignKey('AssetClass', on_delete=models.PROTECT) 
 
-    #资产所属的部门id
-    department = models.BigIntegerField()
+    # 资产所属的部门
+    # 资产必须属于某一个部门，部门被删除时，该部门下必须没有资产
+    department = models.ForeignKey('department.Department', on_delete=models.PROTECT)
     
     #资产名称，同一业务实体内不得重名，不同业务实体间可以
-    name = models.CharField(max_length=100)
-    
-    #资产类型，False为条目型，True为数量型
-    type = models.BooleanField(default=False)
+    name = models.CharField(max_length=128)
     
     #资产数量，条目型始终为1，数量型可变动
     number = models.IntegerField(default=1)
     
-    #挂账人的用户id
-    belonging = models.BigIntegerField()
+    #挂账人
+    # 每个资产都一定要有挂账人，删除一个用户之前一定要保证这个用户下没有挂账的资产，否则这里报错: ProtectedError
+    belonging = models.ForeignKey('user.User', null=False, on_delete=models.PROTECT)
     
     #资产原价值
     price = models.DecimalField(max_digits=10,decimal_places=2)
@@ -41,7 +58,7 @@ class Asset(models.Model):
     additional = models.TextField(default="{}")
     
     #资产的状态，枚举类型，0闲置，1在使用，2维保，3清退，4删除
-    status = models.IntegerField(default=0)
+    status = models.IntegerField(choices=AsserStatus.choices, default=AsserStatus.IDLE)
     
 
     class Meta:
@@ -53,7 +70,6 @@ class Asset(models.Model):
             "parent":self.id,
             "department":self.department,
             "name":self.name,
-            "type":self.type,
             "number":self.number,
             "belonging":self.belonging,
             "price":self.price,
@@ -74,5 +90,7 @@ class AssetClass(models.Model):
     
     name = models.CharField(max_length=128, unique=True, verbose_name="类别名")
     
+    #资产类型，False为条目型，True为数量型
+    type = models.BooleanField(null=False)
     
     
--- asset/views.py
+++ asset/views.py
@@ -1,11 +1,12 @@
 # cyh
 import json
 import re
+import time
 
 from user.models import User
-from department.models import Department
+from department.models import Department, Entity
 from logs.models import Logs
-from asset.models import Asset
+from asset.models import Asset, AssetClass
 
 from utils.utils_request import BAD_METHOD, request_failed, request_success, return_field
 from utils.utils_require import MAX_CHAR_LENGTH, CheckRequire, require
@@ -29,76 +30,134 @@ class asset(APIView):
     def get_object(object:Asset):
         ret = {}
         ret["id"] = object.id
-        ret["parent_id"] = object.parent
-        if ret["parent_id"] != 0:
-            ret["parent"] = Asset.objects.filter(id=ret["parent_id"]).first().name
-        else:
-            ret["parent"] = ""
-        ret["department_id"] = object.department
-        ret["department"] = Department.objects.filter(id=ret["department_id"]).first().name
+        ret["parent"] = object.parent.name if object.parent else ""
+        ret["entity"] = object.entity.name
+        ret["category"] = object.category.name
+        ret["department"] = object.department.name
         ret["name"] = object.name
-        ret["type"] = object.type
         ret["number"] = object.number
-        ret["belonging_id"] = object.belonging
-        ret["belonging"] = User.objects.filter(id=ret["belonging_id"]).first()
+        ret["belonging"] = object.belonging.name
         ret["price"] = object.price
+        ret["life"] = object.life
+        ret["create_time"] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(object.create_time))
         ret["description"] = object.description
         ret["additional"] = json.loads(object.additional)
         ret["status"] = object.status
-        ret["create_time"] = object.create_time
-        ret["life"] = object.life
         
         return ret
     
     @Check
     def get(self, req:Request, format=None):
         object = Asset.objects.all()
-        if "id" in req.data:
-            id = req.data["id"]
+        if "id" in req.query_params:
+            id = require(req.query_params, "id", "int", "Error type of [id]")
             object = object.filter(id=id)
-        if "parent" in req.data:
-            parent = req.data["parent"]
-            parent = Asset.objects.fi
-            object = object.filter(id=id)
+            objects = [self.get_object(obj) for obj in object]
+            return objects
+        if "entity" in req.query_params:
+            entity_name = require(req.query_params, "entity", "string", "Error type of [entity]")
+            entity = Entity.objects.filter(name=entity_name).first()
+            if not entity:
+                raise Failure("所提供的业务实体不存在")
+            object = object.filter(entity=entity)
+            if "parent" in req.query_params:
+                parent = require(req.query_params, "parent", "string", "Error type of [parent]")
+                parent = Asset.objects.filter(name=parent).first()
+                if not parent:
+                    raise Failure("所提供的上级资产不存在")
+                object = object.filter(parent=parent)
+        else:
+            if "parent" in req.query_params:
+                raise Failure("提供了上级资产，却没有指定业务实体")
+        if "category" in req.query_params:
+            cate = require(req.query_params, "category", err_msg="Error type of [category]")
+            cate = AssetClass.objects.filter(name=cate).first()
+            if not cate:
+                raise Failure("所提供的资产类型不存在")
+            object = object.filter(category=cate)
+        if "department" in req.query_params:
+            dep = require(req.query_params, "department", err_msg="Error type of [department]")
+            dep = Department.objects.filter(name=dep).first()
+            if not dep:
+                raise Failure("所提供的部门不存在")
+            object = object.filter(department=dep)
+        if "name" in req.query_params:
+            name = require(req.query_params, "name", err_msg="Error type of [name]")
+            object = object.filter(name=name)
+        if "belonging" in req.query_params:
+            user = require(req.query_params, "belonging", err_msg="Error type of [belonging]")
+            user = User.objects.filter(name=user).first()
+            if not user:
+                raise Failure("所提供的挂账人不存在")
+            object = object.filter(user=user)
+        if "from" in req.query_params:
+            from_ = require(req.query_params, "from", "float", err_msg="Error type of [from]")
+            object = object.filter(from__gte=from_)
+        if "to" in req.query_params:
+            to_ = require(req.query_params, "to", "float", err_msg="Error type of [to]")
+            object = object.filter(to__lte=to_)
+        if "status" in req.query_params:
+            status = require(req.query_params, "status", "int", err_msg="Error type of [status]")
+            object = object.filter(status=status)
+        return Response(object)
         
-            
-        
+               
     @Check   
     def post(self, req:Request):
         if 'parent' in req.data.keys():
-            parent = require(req.data, 'parent', 'int', "Error type of [parent]")
+            parent = require(req.data, 'parent', 'string', "Error type of [parent]")
+            parent = Asset.objects.filter(name=parent).first()
+            if not parent:
+                raise Failure("上级资产不存在")
         else:
-            parent = 0
-        dep = require(req.data, "department", "int", "Missing or error type of [department]")
+            parent = None
+        entity = require(req.data, "entity", "string", "Missing or error type of [entity]")
+        entity = Entity.objects.filter(name=entity).first()
+        if not entity:
+            raise Failure("所属业务实体不存在")
+        category = require(req.data, "category", "string", "Missing or error type of [category]")
+        category = AssetClass.objects.filter(name=category).first()
+        if not category:
+            raise Failure("该资产类型不存在")
+        dep = require(req.data, "department", "string", "Missing or error type of [department]")
+        dep = Department.objects.filter(entity=entity.id, name=dep).first()
+        if not dep:
+            raise Failure("所属部门不存在")
         name = require(req.data, "name", "string", "Missing or error type of [name]")
-        if len(name) > 100:
+        if len(name) > 128:
             raise Failure("名称过长")
-        if 'type' in req.data.keys():
-            tp = require(req.data, 'type', 'bool', "Error type of [type]")
+        if Asset.objects.filter(name=name).first():
+            raise Failure("名称重复")
+        tp = category.type
+        if tp == True:
+            number = require(req.data, "number", "int", "Missing or error type of [number]")
+            if number < 0:
+                raise Failure("数量不能为负数")
         else:
-            tp = False
-        if 'number' in req.data.keys():
-            number = require(req.data, 'number', 'int', "Error type of [number]")
-            if not tp and number != 1:
-                raise Failure("条目型资产的数量应当始终为1")
-            if tp and number <= 0:
-                raise Failure("数量型资产的数量应当至少为1")
-        else:
-            if tp:
-                raise Failure("定义条目型资产应当提供数量")
             number = 1
-        belonging = require(req.data, "belonging", "int", "Missing or error type of [belonging]")
+        if "belonging" in req.data.keys():
+            belonging = require(req.data, "belonging", "string", "Missing or error type of [belonging]")
+            user = User.objects.filter(name=belonging).first()
+            if not user:
+                raise Failure("挂账人不存在")
+        else:
+            user = req.user
         price = require(req.data, "price", "float", "Missing or error type of [price]")
-        life = require(req.data, "life", "int", "Missing or error type of [life]")
+        if "life" in req.data.keys():
+            life = require(req.data, "life", "int", "error type of [life]")
+            if life < 0:
+                raise Failure("使用年限不能为负数")
+        else:
+            life = 0
         if 'description' in req.data.keys():
             description = require(req.data, 'description', 'string', "Error type of [description]")
         else:
             description = ""
         if "additional" in req.data.keys():
             addi = req.data["additional"]
-            if type(addi) is not dict:
-                raise Failure("Error type of [additional]")
             additional = json.dumps(addi)
+            if type(additional) is not dict:
+                raise Failure("Error type of [additional]")
         else:
             additional = json.dumps({})
         
