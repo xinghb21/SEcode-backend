e0d907d0d6cef8d6e1954d59487d36f8d102e7d3
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2023-04-19 15:29:16 +0800

    [B-3-0-NN-013-I]修改数据库建立方式(#13)

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-python3 manage.py makemigrations user
-python3 manage.py makemigrations asset
-python3 manage.py makemigrations department
-python3 manage.py makemigrations pending
-python3 manage.py makemigrations logs
+# python3 manage.py makemigrations user
+# python3 manage.py makemigrations asset
+# python3 manage.py makemigrations department
+# python3 manage.py makemigrations pending
+# python3 manage.py makemigrations logs
+# python3 manage.py makemigrations feishu
 python3 manage.py migrate
 
 # TODO Start: [Student] Run with uWSGI instead
