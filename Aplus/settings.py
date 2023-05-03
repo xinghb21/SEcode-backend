56da56c016623823c5978ce6c0e8c64c5ae696ae
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2023-05-03 20:10:58 +0800

    [B-3-0-NN-017-I]暂时关闭csrf认证保护(#17)

--- Aplus/settings.py
+++ Aplus/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'utils.log_middleware.LogMiddle',
     # Remove CSRF middleware to make it happy, although insecure :(
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
