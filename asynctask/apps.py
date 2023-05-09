d4acf16298553d360d31fac422c04ca80108108d
cyh2004 <chenying21@mails.tsinghua.edu.cn>
2023-05-09 23:24:24 +0800

    [B-4-0-NN-023-I]修改asynctask的app配置(#23)

--- asynctask/apps.py
+++ asynctask/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AsyncConfig(AppConfig):
+class AsynctaskConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'asynctask'
