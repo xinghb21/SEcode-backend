7a455d0908189226ae66b097179d1181979f2cbb
韩宇轩 <hanyx21@mails.tsinghua.edu.cn>
2023-05-11 15:17:59 +0800

    [B-4-0-EP-022-I]修改扫描二维码获取的信息接口(#22)

--- asset/views.py
+++ asset/views.py
@@ -409,81 +409,12 @@ def getdetail(req:Request):
 
 #资产全视图，标签二维码显示，不需要登录
 @CheckRequire
-def fulldetail(req:HttpRequest,id:any):
+def fulldetail(req:Request,id:any):
     asset = Asset.objects.filter(id=int(id)).exclude(status=4).first()
     if not asset:
-        return HttpResponse("资产不存在")
-    content = "<h4>基本信息<h4/>"
-    content += "资产名称:" + asset.name + '<br/>'
-    content += "资产编号:" + str(asset.id) + '<br/>'
-    content += "业务实体:" + asset.entity.name + '<br/>'
-    content += "所属部门:" + asset.department.name + '<br/>'
-    content += "创建时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(asset.create_time)) + '<br/>'
-    if asset.category != None:
-        content += "资产类别:" + asset.category.name + "(" + ("数量型" if asset.type else "条目型") + ")" + '<br/>'
-    else:
-        content += "资产类别:" + ("数量型" if asset.type else "条目型") + ",尚未确定具体类别"+ '<br/>'
-    if asset.parent != None:
-        content += "上级资产:" + asset.parent.name + '<br/>'
-    if asset.belonging != None:
-        content += "挂账人:" + asset.belonging.name + '<br/>'
-    
-    content += "原市值:" + str(float(asset.price)) + '<br/>'
-    content += "描述信息:" + (asset.description if asset.description else "暂无描述") + '<br/>'
-    content += "html格式补充说明:" + (asset.additionalinfo if asset.additionalinfo else "暂无描述") + '<br/>'
-    addition = json.loads(asset.additional)
-    if addition:
-        for key in addition:
-            content += "%s:%s" % (key,addition[key])  + '<br/>'
-    content += "<h4>使用情况<h4/>"
-    if asset.expire:
-        content += "已报废<br/>"
-    elif asset.type:
-        content += "总数量:" + str(asset.number) + '<br/>'
-        content += "闲置数量:" + str(asset.number_idle) + '<br/>'
-        content += "清退数量:" + str(asset.number_expire) + '<br/>'
-        usage = json.loads(asset.usage)
-        maintain = json.loads(asset.maintain)
-        process = json.loads(asset.process)
-        if usage:
-            content += "<h5>使用<h5/>"
-            for user in usage:
-                content += "%s:%d" % (list(user.keys())[0],user[list(user.keys())[0]]) + '<br/>'
-        if maintain:
-            content += "<h5>维保<h5/>"
-            for user in maintain:
-                content += "%s:%d" % (list(user.keys())[0],user[list(user.keys())[0]]) + '<br/>'
-        if process:
-            content += "<h5>入库审批<h5/>"
-            for user in process:
-                content += "%s:%d" % (list(user.keys())[0],user[list(user.keys())[0]]) + '<br/>'
-    else:
-        content += "使用者:" + (asset.user.name if asset.user else "无") + '<br/>'
-        content += "状态:" + ("闲置" if asset.status == 0 else ("使用" if asset.status == 1 else("维保" if asset.status == 2 else ("清退") if asset.status == 3 else ("废弃") if asset.status == 4 else "入库审批"))) + '<br/>'
-    logs = list(AssetLog.objects.filter(asset=asset).all().order_by("-time"))[0:30:]
-    content += "<h4>调动历史<h4/>"
-    for log in logs:
-        if log.type == 1:
-            if log.dest:
-                content += "用户%s从外部门获取,数量:%d" % (log.dest.name,log.number) + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-            else:
-                content += "资产管理员导入,数量:%d" % log.number + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-        if log.type == 2:
-            content += "用户%s领用,数量:%d" % (log.dest.name,log.number) + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-        if log.type == 3:
-            content += "用户%s向用户%s转移,数量:%d" % (log.src.name,log.dest.name,log.number) + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-        if log.type == 4:
-            content += "用户%s申请维保,数量:%d" % (log.src.name,log.number) + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-        if log.type == 5:
-            content += "用户%s维保完成并返还,数量:%d" % (log.dest.name,log.number) + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-        if log.type == 6:
-            content += "用户%s退库,数量:%d" % (log.src.name,log.number) + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-        if log.type == 7:
-            content += "用户%s向外部门用户%s转移,数量:%d" % (log.src.name,log.dest.name,log.number) + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-        if log.type == 8:
-            content += "资产被手动删除" + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-        if log.type == 9:
-            content += "资产闲置数量更改为%d" % (log.number) + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-        if log.type == 10:
-            content += "用户%s申请维保的资产报废,数量:%d" % (log.src.name,log.number) + ",时间:" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(log.time)) + '<br/>'
-    return HttpResponse(content)
+        raise Failure("该资产不存在")
+    ret = {
+        "code": 0,
+        "data":asset.serialize(),
+    }
+    return Response(ret)
