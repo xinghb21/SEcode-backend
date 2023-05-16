f2509a089041dfd2109215a6bd724ca39b40b805
韩宇轩 <hanyx21@mails.tsinghua.edu.cn>
2023-05-16 10:08:50 +0800

    [B-4-0-EP-022-B]取消用户拥有资产的列表分页(#22)

--- user/ns/views.py
+++ user/ns/views.py
@@ -276,9 +276,7 @@ class NsViewSet(viewsets.ViewSet):
     @Check
     @action(detail=False,methods=['get'],url_path="possess")
     def possess(self,req:Request):
-        page = int(req.query_params["page"])
         list = self.staffassets(req.user.name)
-        list = list[10*page-10:10*page:]
         ent = Entity.objects.filter(id=req.user.entity).first()
         dep = Department.objects.filter(id=req.user.department).first()
         return Response({"code":0,"assets":list,"entity":ent.name,"department":dep.name})
